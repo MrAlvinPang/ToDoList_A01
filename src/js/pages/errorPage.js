9b00e9b54f76d9acc304a572970effa1623c9c8e
MrAlvinPang <71090757+MrAlvinPang@users.noreply.github.com>
2021-10-19 20:54:27 -0600

    Error page complete with link home

--- src/js/pages/errorPage.js
+++ src/js/pages/errorPage.js
@@ -1,7 +1,21 @@
+import heading from "../components/ui/heading";
+import makeElement from "../utils/makeElement";
+import logo from "../components/icons/logo";
+import header from "../components/header/header";
 import link from "../components/ui/link";
 
 const errorPage = function(){
+    const errorHeader = document.createElement('header')
+    errorHeader.classList.add('error-page')
+    const h1 = heading('h1', 'Something went wrong :(')
+    const linkElem = link('Take me home', '/home')
+
+    errorHeader.appendChild(makeElement(logo()))
+    errorHeader.appendChild(makeElement(header('Your favorite daily tracker')))
     
+    errorHeader.appendChild(h1)
+    errorHeader.appendChild(linkElem)
+    return errorHeader
 }
 
 export default errorPage
--- src/js/pages/homePage.js
+++ src/js/pages/homePage.js
@@ -2,12 +2,15 @@ import heading from "../components/ui/heading";
 import makeElement from "../utils/makeElement";
 import logo from "../components/icons/logo";
 import header from "../components/header/header";
+import link from "../components/ui/link";
 
 const homePage = function(){
     const homeHeader = document.createElement('header')
     homeHeader.classList.add('home-page')
     const h1 = heading('h1', 'What\'s up, Alvin!')
+    const linkElem = link("Error Page", "/error")
 
+    homeHeader.append(linkElem)
     homeHeader.append(h1)
     homeHeader.appendChild(makeElement(logo()))
     homeHeader.appendChild(makeElement(header('Your favorite daily tracker')))
