b612b8f343001f052689bd01f913ceeb16df1680
MrAlvinPang <71090757+MrAlvinPang@users.noreply.github.com>
2021-10-22 03:35:39 -0600

    More minor adjustments to style

--- src/js/components/cards/todoitem.js
+++ src/js/components/cards/todoitem.js
@@ -5,7 +5,7 @@ const items = function({id, category, description, isComplete, endDate}){
     <li class="todoitems" data-key="${id}">
     <p class="card-title"> ${category} </p>
     <p class="card-desc"> ${description}</p>
-    <p class="card-comp">Complete: ${isComplete}</p>
+    <p class="card-comp">Comp: ${isComplete}</p>
     <p class="card-date">Due: ${endDate}</p>
     <p class="controls"><button>edit</button><button>delete</button></p> 
     </li>
